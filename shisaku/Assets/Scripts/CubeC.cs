a918d5098bb89cde3c8318737c8ded897b8b7d3b
PG22-01 <>
2023-05-06 17:50:41 +0900

    移動、落下

---
+++ shisaku/Assets/Scripts/CubeC.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeC : MonoBehaviour
+{
+
+    public GroundCheck ground;
+    public PlayerController playercontroller;
+
+    private bool isGround = false;
+    private float Speed;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        Speed = playercontroller.PlayerSpeed;
+    }
+    private void FixedUpdate()
+    {
+        isGround = ground.IsGround();
+        Gravity();
+    }
+
+
+
+    private void Gravity()
+    {
+        if (isGround == false)
+        {
+            transform.position += new Vector3(0, -0.2f, 0);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.position += new Vector3(Speed, 0, 0);
+            }
+        }
+    }
+
+}
---
+++ shisaku/Assets/Scripts/WallGeter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGeter : MonoBehaviour
+{
+    private bool RightWall;
+    private bool LeftWall;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        
+    }
+}
