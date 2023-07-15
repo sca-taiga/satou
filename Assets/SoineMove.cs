4a2892cd5405c7705c912f231fb6dbd28d3d499c
sasaki <>
2023-07-15 18:07:02 +0900

    スパインアニメーション切り替えジャンプと歩く完了

--- Assets/SoineMove.cs
+++ Assets/SoineMove.cs
@@ -2,6 +2,7 @@ using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine;
 
 /// <summary> Spineアニメーションの再生を確認サンプルクラス </summary>
 public class SoineMove : MonoBehaviour
@@ -10,15 +11,29 @@ public class SoineMove : MonoBehaviour
 	/// <summary> 再生するアニメーション名 </summary>
 	[SerializeField]
 	private string testAnimationName = "jump/jump";
+
 	[SerializeField]
 	private string testAnimationName2 = "walk/aruku";
 
+	[SerializeField]
+	private string testAnimationName3 = "walk/aruku";
+
+	[SerializeField]
+	private string testAnimationName4 = "pushpull/pull";
+
+	[SerializeField]
+	private string testAnimationName5 = "pushpull/push";
+
+	private bool JumpFlagg = false;
+	private bool WoakFlagg = false;
+	private bool PushpullFlagg = false;
 	/// <summary> ゲームオブジェクトに設定されているSkeletonAnimation </summary>
 	private SkeletonAnimation skeletonAnimation = default;
 
 	/// <summary> Spineアニメーションを適用するために必要なAnimationState </summary>
 	private Spine.AnimationState spineAnimationState = default;
 	//var 
+	
 
 	private void Start()
 	{
@@ -35,29 +50,93 @@ public class SoineMove : MonoBehaviour
 		//Spaceキーでジャンプ
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-
+			JumpFlagg = true;
 			PlayAnimation();
-
 		}
 		//Aキーで左移動
         else if (Input.GetKeyDown(KeyCode.A))
         {
+			//WorkAnimation();
+			WoakFlagg = true;
 			PlayAnimation();
-        }
-
-        
-    }
+		}
+		//Dキーで左移動
+		else if (Input.GetKeyDown(KeyCode.D))
+		{
+			//WorkAnimation();
+			WoakFlagg = true;
+			PlayAnimation();
+		}
+		//shift+Aキーで物を押す引く
+		else if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			//WorkAnimation();
+			PushpullFlagg = true;
+			PlayAnimation();
+		}
+		//shift+Dキーで物を押す引く
+		else if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			//WorkAnimation();
+			PushpullFlagg = true;
+			PlayAnimation();
+		}
 
 	/// <summary>
 	/// Spineアニメーションを再生
 	/// testAnimationNameに再生したいアニメーション名を記載してください。
 	/// </summary>
-	private void PlayAnimation()
+	  void PlayAnimation()
+	{
+		if(JumpFlagg)
+        {
+			// アニメーション「testAnimationName」を再生
+			spineAnimationState.SetAnimation(0, testAnimationName, true);
+			JumpFlagg = false;
+		}
+		if(WoakFlagg)
+        {
+			// アニメーション「testAnimationName」を再生
+			spineAnimationState.SetAnimation(0, testAnimationName2, true);
+			WoakFlagg = false;
+		}
+		if (WoakFlagg)
+		{
+			// アニメーション「testAnimationName」を再生
+			spineAnimationState.SetAnimation(0, testAnimationName3, true);
+			WoakFlagg = false;
+		}
+		if (PushpullFlagg)
+			// アニメーション「testAnimationName」を再生
+			spineAnimationState.SetAnimation(0, testAnimationName4, true);
+		PushpullFlagg = false;
+	}
+		if (PushpullFlagg)
+			// アニメーション「testAnimationName」を再生
+			spineAnimationState.SetAnimation(0, testAnimationName5, true);
+		PushpullFlagg = false;
+		/*
+		private void WorkAnimation()
+		{
+			// アニメーション「testAnimationName」を再生
+			spineAnimationState.SetAnimation(0, testAnimationName2, true);
+		}
+		*/
+		 void PlayAnimation2()
 	{
 		// アニメーション「testAnimationName」を再生
-		spineAnimationState.SetAnimation(0, testAnimationName, true);
-		// アニメーション「testAnimationName」を再生
-		spineAnimationState.SetAnimation(0, testAnimationName2, true);
+		TrackEntry trackEntry = spineAnimationState.SetAnimation(0, testAnimationName, true);
+
+		// 完了通知を取得準備
+		trackEntry.Complete += OnSpineComplete;
 	}
+
+	 void OnSpineComplete(TrackEntry trackEntry)
+	{
+		// アニメーション完了時に行う処理を記載
+		spineAnimationState.SetAnimation(0, testAnimationName, true);
+	}
+
+}
 }
 
