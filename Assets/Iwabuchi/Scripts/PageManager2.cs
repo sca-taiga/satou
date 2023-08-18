91d12fbf3742201e9337216213eaa730f0dee3ed
2PC-23\student <20cge0007kr@edu.sendai-com.ac.jp>
2023-08-18 17:19:50 +0900

    ページ選択完成

---
+++ Assets/Iwabuchi/Scripts/PageManager2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageManager2 : MonoBehaviour
+{
+    private int nowPage;
+    [SerializeField] GameObject PageOne;
+    [SerializeField] GameObject PageTwo;
+    [SerializeField] GameObject PageThree;
+
+    private void Start()
+    {
+        nowPage = 1;
+        UpdatePageVisibility();
+    }
+
+    private void UpdatePageVisibility()
+    {
+        PageOne.SetActive(nowPage == 1);
+        PageTwo.SetActive(nowPage == 2);
+        PageThree.SetActive(nowPage == 3);
+    }
+
+    public void OnClickRightButton()
+    {
+        nowPage = (nowPage % 3) + 1; // ページ番号を1から3までループ
+        UpdatePageVisibility();
+        Debug.Log("ページ切り替え: nowPage = " + nowPage);
+    }
+}
