1467d7bb4b0c8b76bb13b8420d0ebb6532970e5b
sca-taiga <104498281+sca-taiga@users.noreply.github.com>
2023-09-08 02:55:15 +0900

    プランナーさんからの追加要望UIやscene制作プログラムなど

--- Assets/taiga/Scripts/stegeScenn.cs
+++ Assets/taiga/Scripts/stegeScenn.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class stegeScenn : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,9 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
